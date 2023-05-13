bfad593cbc931767227b1ba084bdecb3f4ae276d
Giorgio Garofalo <giogar2705@gmail.com>
2023-05-13 17:41:54 +0200

    Make the models pane scrollable

--- src/main/kotlin/rubiksworld/view/shop/ShopView.kt
+++ src/main/kotlin/rubiksworld/view/shop/ShopView.kt
@@ -1,6 +1,7 @@
 package rubiksworld.view.shop
 
 import javafx.geometry.Orientation
+import javafx.scene.control.ScrollPane
 import javafx.scene.layout.FlowPane
 import javafx.scene.layout.Pane
 import javafx.scene.layout.VBox
@@ -9,6 +10,8 @@ import rubiksworld.view.ModelCard
 import rubiksworld.view.View
 
 const val CURRENCY = '€'
+
+private const val BARS_HEIGHT = 57.0
 
 /**
  * The shop section content.
@@ -20,7 +23,7 @@ class ShopView(val onUpdate: () -> Unit) : View<Pane> {
     override fun create(controller: Controller) = VBox().apply {
         val modelsPane = FlowPane(Orientation.HORIZONTAL).apply { styleClass += "models-pane" }
 
-        children += ShopBar { filters ->
+        val bar = ShopBar { filters ->
             controller.async {
                 val results = searchModels(filters)
                 controller.sync {
@@ -30,6 +33,11 @@ class ShopView(val onUpdate: () -> Unit) : View<Pane> {
             }
         }.create(controller)
 
-        children += modelsPane
+        val scrollPane = ScrollPane(modelsPane).apply {
+            isFitToWidth = true
+            prefHeightProperty().bind(sceneProperty().map { it?.height?.minus(BARS_HEIGHT) ?: 0 })
+        }
+
+        children.addAll(bar, scrollPane)
     }
 }
