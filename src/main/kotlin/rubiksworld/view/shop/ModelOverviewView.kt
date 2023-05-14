a1ea84ca3e2e63c54f11b464ed8f79932eac6ca7
Giorgio Garofalo <giogar2705@gmail.com>
2023-05-14 21:54:27 +0200

    Set-up model overview

--- src/main/kotlin/rubiksworld/view/shop/ModelOverviewView.kt
+++ src/main/kotlin/rubiksworld/view/shop/ModelOverviewView.kt
@@ -18,6 +18,22 @@ class ModelOverviewView(private val model: Model) : View<Pane> {
     }
 
     private fun createRightColumn(controller: Controller) = VBox().apply {
-        children.addAll(controller.getCustomizableParts(model).map { Label(it.part) })
+        styleClass += "model-overview"
+
+        fun title(text: String) = Label(text).apply { styleClass += "section-title" }
+
+        children += title("${model.maker} ${model.name}")
+
+        children += title("Category")
+        children += Label(model.category.name)
+
+        children += title("Tags")
+        // TODO tags
+
+        val parts = controller.getCustomizableParts(model)
+
+        parts.forEach { part ->
+            children += title(part.part)
+        }
     }
 }
