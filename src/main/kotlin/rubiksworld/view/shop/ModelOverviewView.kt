2726dab94f967b225958a540d07cae48abe03812
Giorgio Garofalo <giogar2705@gmail.com>
2023-05-14 22:39:55 +0200

    Build full model overview

--- src/main/kotlin/rubiksworld/view/shop/ModelOverviewView.kt
+++ src/main/kotlin/rubiksworld/view/shop/ModelOverviewView.kt
@@ -1,12 +1,19 @@
 package rubiksworld.view.shop
 
 import javafx.scene.control.Label
+import javafx.scene.control.RadioButton
+import javafx.scene.control.ToggleGroup
+import javafx.scene.image.ImageView
 import javafx.scene.layout.HBox
 import javafx.scene.layout.Pane
 import javafx.scene.layout.VBox
 import rubiksworld.controller.Controller
+import rubiksworld.model.Customization
 import rubiksworld.model.Model
+import rubiksworld.view.ImageCache
 import rubiksworld.view.View
+
+private const val IMAGE_WIDTH = 250.0
 
 /**
  *
@@ -14,12 +21,15 @@ import rubiksworld.view.View
 class ModelOverviewView(private val model: Model) : View<Pane> {
 
     override fun create(controller: Controller) = HBox().apply {
+        styleClass += "model-overview"
+        children += ImageView(ImageCache[model.imageUrl]).apply {
+            isPreserveRatio = true
+            fitWidth = IMAGE_WIDTH
+        }
         children += createRightColumn(controller)
     }
 
     private fun createRightColumn(controller: Controller) = VBox().apply {
-        styleClass += "model-overview"
-
         fun title(text: String) = Label(text).apply { styleClass += "section-title" }
 
         children += title("${model.maker} ${model.name}")
@@ -28,12 +38,44 @@ class ModelOverviewView(private val model: Model) : View<Pane> {
         children += Label(model.category.name)
 
         children += title("Tags")
-        // TODO tags
+        children += Label(getTags().joinToString(", "))
 
         val parts = controller.getCustomizableParts(model)
 
         parts.forEach { part ->
             children += title(part.part)
+
+            val customizations = controller.getAvailableCustomizations(part)
+            children += createCustomizationsBox(customizations)
+        }
+    }
+
+    private fun createCustomizationsBox(customizations: List<Customization>) = HBox().apply {
+        styleClass += "customizations-box"
+        val group = ToggleGroup()
+
+        val buttons = customizations.map { customization ->
+            RadioButton(customization.change).apply {
+                toggleGroup = group
+                isSelected = customization.isDefault
+            }
+        }
+
+        children.addAll(buttons)
+    }
+
+    private fun getTags(): List<String> = buildList {
+        if (model.isSpeedCube) {
+            add("Speed Cube")
+        }
+        if (model.isStickerless) {
+            add("Stickerless")
+        }
+        if (model.isMagnetic) {
+            add("Magnetic")
+        }
+        if (isEmpty()) {
+            add("None")
         }
     }
 }
