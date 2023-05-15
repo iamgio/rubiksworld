8cf21bbcfb04674ad05af416906898a0a2c6b5df
Giorgio Garofalo <giogar2705@gmail.com>
2023-05-15 13:45:43 +0200

    Add CartPresence entity

---
+++ src/main/kotlin/rubiksworld/controller/database/tables/CartPresences.kt
@@ -0,0 +1,15 @@
+package rubiksworld.controller.database.tables
+
+import org.ktorm.schema.Table
+import org.ktorm.schema.int
+import org.ktorm.schema.varchar
+import rubiksworld.model.CartPresence
+
+/**
+ * Cart presences DB table.
+ */
+object CartPresences : Table<CartPresence>("CartPresences") {
+
+    val userNickname = varchar("user_nickname").primaryKey().bindTo { it.user.nickname }
+    val modelVersionId = int("model_version_id").primaryKey().bindTo { it.modelVersion.id }
+}
---
+++ src/main/kotlin/rubiksworld/model/CartPresence.kt
@@ -0,0 +1,21 @@
+package rubiksworld.model
+
+import org.ktorm.entity.Entity
+
+/**
+ * Presence of a model version in a user's cart.
+ */
+interface CartPresence : Entity<CartPresence> {
+
+    companion object : Entity.Factory<CartPresence>()
+
+    /**
+     * Cart's owner.
+     */
+    val user: User
+
+    /**
+     * Target model.
+     */
+    val modelVersion: ModelVersion
+}
