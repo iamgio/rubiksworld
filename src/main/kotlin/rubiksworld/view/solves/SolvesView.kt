512f5c63783b8fdc62e71309522ef2ad630cba87
Giorgio Garofalo <giogar2705@gmail.com>
2023-05-19 15:18:01 +0200

    Set-up Solves table

--- src/main/kotlin/rubiksworld/view/solves/SolvesView.kt
+++ src/main/kotlin/rubiksworld/view/solves/SolvesView.kt
@@ -1,9 +1,15 @@
 package rubiksworld.view.solves
 
+import javafx.beans.property.SimpleStringProperty
+import javafx.scene.control.TableColumn
+import javafx.scene.control.TableView
 import javafx.scene.layout.Pane
 import javafx.scene.layout.VBox
 import rubiksworld.controller.Controller
+import rubiksworld.model.Solve
+import rubiksworld.model.SolveTime
 import rubiksworld.view.View
+import java.time.format.DateTimeFormatter
 
 /**
  * The Solves section content.
@@ -14,5 +20,34 @@ class SolvesView : View<Pane> {
         styleClass += "solves-view"
 
         children += SolvesBar().create(controller)
+
+        val table = TableView<Solve>()
+
+        table.columns.addAll(
+            column("Position") { (table.items.indexOf(it) + 1).toString() },
+            column("User") { it.user.nickname },
+            column("Time") { formatTime(it.solveTime) },
+            column("Model") {
+                it.model?.let { model -> "${model.maker} ${model.name}" } ?: ""
+            },
+            column("Date") {
+                it.registrationDate.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"))
+            }
+        )
+
+        table.items.setAll(controller.getSolves(controller.user))
+
+        children += VBox(table).apply { styleClass += "table-container" }
+    }
+
+    private fun column(name: String, binding: (Solve) -> String) = TableColumn<Solve, String>(name).apply {
+        setCellValueFactory { SimpleStringProperty(binding(it.value)) }
+    }
+
+    private fun formatTime(time: SolveTime) = buildString {
+        if (time.minutes > 0) {
+            append(time.minutes).append("m ")
+        }
+        append(time.seconds).append("s")
     }
 }
